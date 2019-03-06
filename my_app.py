c9ed39a12c7aa73cdf384bda7dab7fefa6793939
Rachael Tatman <rctatman@users.noreply.github.com>
2019-03-06 15:05:26 -0800

    added upload route

--- my_app.py
+++ my_app.py
@@ -4,3 +4,9 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+@app.route('/upload', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        f = request.files['the_file']
+        f.save('/var/www/uploads/uploaded_file.txt')
