d8461fe398ba4e64eecc8825879f9d13c826b4f8
Rachael Tatman <rctatman@users.noreply.github.com>
2019-03-07 14:50:33 -0800

    removed auth stuff (out of scope)

--- script.py
+++ script.py
@@ -3,23 +3,8 @@ import flask
 from flask import Flask, jsonify, request
 from serve import get_keywords_api
 import json
-from flask_httpauth import HTTPBasicAuth
 
 app=Flask(__name__)
-auth = HTTPBasicAuth()
-
-# plain text user data
-# DO NOT DO THIS IN PRODUCTION
-USER_DATA = {
-    "user1": "password"
-}
-
-# fucntion to varify password passed through API
-@auth.verify_password
-def verify(username, password):
-    if not (username and password):
-        return False
-    return USER_DATA.get(username) == password
 
 # this will print some text at our app's URL
 @app.route('/')
@@ -31,7 +16,6 @@ keywords_api = get_keywords_api()
 
 # API route
 @app.route('/api', methods=['POST'])
-@auth.login_required
 def api():
     """API function
     All model-specific logic to be defined in the get_model_api()
