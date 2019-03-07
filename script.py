482fd84bda59c175044d64f56a72001e64ecc864
Rachael Tatman <rctatman@users.noreply.github.com>
2019-03-06 16:22:57 -0800

    added input page

--- script.py
+++ script.py
@@ -1,6 +1,6 @@
 #importing libraries
 import flask
-from flask import Flask
+from flask import Flask, jsonify, request
 
 app=Flask(__name__)
 
@@ -8,3 +8,17 @@ app=Flask(__name__)
 @app.route('/index')
 def index():
     return flask.render_template('index.html')
+
+# page to accept inputs
+@app.route('/api_input')
+def index():
+    return flask.render_template('simple_client.html')
+
+
+# right now this just returns the data you give it
+@app.route('/api', methods=['POST'])
+def api():
+    input_data = request.json
+    output_data = input_data
+    response = jsonify(output_data)
+    return response
