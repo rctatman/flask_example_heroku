5fe14eb0c632950cceb83314623be965d5c173aa
Rachael Tatman <rctatman@users.noreply.github.com>
2019-03-07 11:25:38 -0800

    refactoring api
    modelled after https://github.com/guillaumegenthial/api_ner

--- serve.py
+++ serve.py
@@ -2,10 +2,12 @@ from flashtext.keyword import KeywordProcessor
 import pickle
 
 # funtion that takes in a text string & returns "NER" output
-def get_keywords(post):
+def get_keywords_api():
     # read in pickled word processor
     with open("word_processor.pkl", "rb") as file:
         keyword_processor = pickle.load(file)
     
-    keywords_found = keyword_processor.extract_keywords(post, span_info=True)
-    return keywords_found
+    # get our keywords 
+    def keywords_api(post): 
+        keywords_found = keyword_processor.extract_keywords(post, span_info=True)
+        return keywords_found
